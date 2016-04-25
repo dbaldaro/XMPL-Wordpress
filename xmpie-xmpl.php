14bf93224653f7687e4a49adbba7d904c0fd4510
danny <danny@xmpie.com>
2016-04-25 08:32:21 +0300

    update reference to latest xmpl 1.0.9

--- xmpie-xmpl.php
+++ xmpie-xmpl.php
@@ -3,7 +3,7 @@
 Plugin Name: XMPie XMPL
 Plugin URI: http://xmpie.com/
 Description: XMPie XMPL for WordPress
-Version: 1.0 (1.0.8)
+Version: 1.0 (1.0.9)
 Author: Galit Zwickel
 Author URI: http://www.xmpie.com/
 License: MIT
@@ -18,13 +18,13 @@ register_deactivation_hook(__FILE__, 'xmpie_xm_deactivation');
 
 add_action('wp_enqueue_scripts', 'xmpie_xm_scripts');
 function xmpie_xm_scripts() {
-    wp_register_script('xmpcfg', plugins_url('xmpcfg.js', __FILE__));
-    wp_enqueue_script('xmpcfg');
+	wp_register_script('xmpcfg', plugins_url('xmpcfg.js', __FILE__));
+	wp_enqueue_script('xmpcfg');
 
-    $access_token = (get_option('xmpie_access_token') != '') ? get_option('xmpie_access_token') : '';
-    $xmpurl = (get_option('xmpie_url') != '') ? get_option('xmpie_url') : '';
-    $circle_project_id = (get_option('xmpie_circle_project_id') != '') ? get_option('xmpie_circle_project_id') : '';
-    $circle_project_name = (get_option('xmpie_circle_project_name') != '') ? get_option('xmpie_circle_project_name') : '';
+	$access_token = (get_option('xmpie_access_token') != '') ? get_option('xmpie_access_token') : '';
+	$xmpurl = (get_option('xmpie_url') != '') ? get_option('xmpie_url') : '';
+	$circle_project_id = (get_option('xmpie_circle_project_id') != '') ? get_option('xmpie_circle_project_id') : '';
+	$circle_project_name = (get_option('xmpie_circle_project_name') != '') ? get_option('xmpie_circle_project_name') : '';
 
 	$config_array = array(
 		'access_token' => $access_token,
@@ -33,22 +33,22 @@ function xmpie_xm_scripts() {
 		'circle_project_name' => $circle_project_name
 	);
 	
-  wp_register_script('jqueryxmp', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.8/jquery/jquery-1.10.2.min.js');
-  wp_enqueue_script('jqueryxmp');
-  wp_localize_script('xmpcfg', 'setting', $config_array);
+	wp_register_script('jqueryxmp', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.9/jquery/jquery-1.10.2.min.js');
+	wp_enqueue_script('jqueryxmp');
+	wp_localize_script('xmpcfg', 'setting', $config_array);
 
-  wp_register_script('xmpjs', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.8/xmp/js/xmp.min.js');
-  wp_enqueue_script('xmpjs');
-  
-  wp_register_script('ucreate_xm_designjs', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.8/xmp/js/ucreateXMDesign.js');
-  wp_enqueue_script('ucreate_xm_designjs');
+	wp_register_script('xmpjs', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.9/xmp/js/xmp.min.js');
+	wp_enqueue_script('xmpjs');
+
+	wp_register_script('ucreate_xm_designjs', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.9/xmp/js/ucreateXMDesign.js');
+	wp_enqueue_script('ucreate_xm_designjs');
 }
 
 add_action('wp_enqueue_scripts', 'xmpie_xm_styles');
 function xmpie_xm_styles() {
-  wp_register_style('xmpie_styles', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.8/xmp/css/xmp.css');
+  wp_register_style('xmpie_styles', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.9/xmp/css/xmp.css');
   wp_enqueue_style('xmpie_styles');
-  wp_register_style('xmpie_ucreateXMDesign', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.8/xmp/css/ucreateXMDesign.css');
+  wp_register_style('xmpie_ucreateXMDesign', 'https://ajax.xmcircle.com/ajax/libs/xmpl/1.0.9/xmp/css/ucreateXMDesign.css');
   wp_enqueue_style('xmpie_ucreateXMDesign');  
 }
 
